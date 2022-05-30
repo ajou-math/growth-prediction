20a4e7edd0e6400d49dcb345f09ebed07eb8d871
Choi Hyoung Jun <chsam2@ajou.ac.kr>
2022-05-30 19:21:34 +0900

    0530

--- src/springboot/src/main/java/com/example/springboot/controller/ReportController.java
+++ src/springboot/src/main/java/com/example/springboot/controller/ReportController.java
@@ -102,6 +102,7 @@ public class ReportController {
         String jsonlink = "http://127.0.0.1:5000/tospring/" + image.getFilename() +
                 "/" + male + "/"
                 + resultDTO.getChildheight();
+        // http://3.92.110.182:5000/tospring/
         URL jsonurl = new URL(jsonlink);
         HttpURLConnection conn = (HttpURLConnection) jsonurl.openConnection();
         StringBuffer stb = new StringBuffer();
